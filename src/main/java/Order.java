326ff9d43d1b23ba76f20006917bde25b8ad21f8
TheLucasAnderson <143748766+TheLucasAnderson@users.noreply.github.com>
2025-01-29 00:19:11 -0300

    Create Order class file

---
+++ src/main/java/Order.java
@@ -0,0 +1,12 @@
+import java.math.BigDecimal;
+
+public class Order {
+    private final String userID, orderId;
+    private final BigDecimal amount;
+
+    public Order(String userId, String orderId, BigDecimal amount) {
+        this.userID = userId;
+        this.orderId = orderId;
+        this.amount = amount;
+    }
+}
