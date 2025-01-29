e2d8d72500434e53ab0c384f6b92e62c9b6bad2b
TheLucasAnderson <143748766+TheLucasAnderson@users.noreply.github.com>
2025-01-29 00:20:25 -0300

    Apply generics in KafkaDispatcher on NewOrderMain

--- src/main/java/NewOrderMain.java
+++ src/main/java/NewOrderMain.java
@@ -1,16 +1,22 @@
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (var dispatcher = new KafkaDispatcher()) {
-            for (var i = 0; i < 10; i++) {
-                var key = UUID.randomUUID().toString();
-                var value = "123,456,789";
-                dispatcher.send("VIRTUAL_STORE_NEW_ORDER", key, value);
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
+                for (var i = 0; i < 10; i++) {
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
 
-                var email = "teste@teste.com.br";
-                dispatcher.send("VIRTUAL_STORE_SEND_EMAIL", key, email);
+                    var order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("VIRTUAL_STORE_NEW_ORDER", userId, order);
+
+                    var email = "test@test.com.br";
+                    emailDispatcher.send("VIRTUAL_STORE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
